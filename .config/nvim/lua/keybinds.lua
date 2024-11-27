e4b92b04012048273322218c6c944c360ba8d671
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2024-11-27 23:04:44 +0100

    Added the next and previous definition keybinds

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -7,3 +7,6 @@ vim.keymap.set("n", "<C-_>", commentApi.toggle.linewise.current)
 vim.keymap.set("i", "<C-_>", commentApi.toggle.linewise.current)
 -- This works and I have no clue how to do it with the dot syntax
 vim.keymap.set("v", "<C-_>", '<Plug>(comment_toggle_blockwise_visual)')
+
+vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })

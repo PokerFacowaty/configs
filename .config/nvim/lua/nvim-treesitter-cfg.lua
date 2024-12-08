0302312303c975c06a9372615f60d0bdef55bfc1
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2024-12-08 21:42:10 +0100

    Added GQL to treesitter config

--- .config/nvim/lua/nvim-treesitter-cfg.lua
+++ .config/nvim/lua/nvim-treesitter-cfg.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup({
   ensure_installed = { "bash", "c", "comment", "csv", "dockerfile", "go",
-                       "html", "htmldjango", "javascript", "json", "lua",
-                       "python", "typescript", "sql" },
+                       "graphql", "html", "htmldjango", "javascript", "json",
+                       "lua", "python", "typescript", "sql" },
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally

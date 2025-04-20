51dedddd6a6b4ab85f41a6c3c1a6bdc3e90ab9ed
Bartłomiej "PokerFacowaty" Nowak <pokerfacowaty.bn@gmail.com>
2025-04-20 21:17:56 +0200

    Sorted aliases

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -3,16 +3,16 @@ then
     eval $(thefuck --alias)
 fi
 
+alias config='/usr/bin/git --git-dir=$HOME/.configs/ --work-tree=$HOME'
 alias f="fuck"
-alias ss="ssh pokerfacowaty@212.7.200.55 -p 2000"
-alias p="python3"
-alias ll="ls -alh"
 alias lh="ls -alth |head"
+alias ll="ls -alh"
 alias n="nvim"
+alias p="python3"
+alias ss="ssh pokerfacowaty@212.7.200.55 -p 2000"
 alias v="vim"
 
 # Main PC aliases
 alias ebook-convert="/var/lib/flatpak/app/com.calibre_ebook.calibre/x86_64/stable/active/files/lib/calibre/ebook-convert"
 
-alias config='/usr/bin/git --git-dir=$HOME/.configs/ --work-tree=$HOME'
 

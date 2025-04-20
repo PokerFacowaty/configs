48d8dbd212a635eb7caeac9483946649da6bb2d5
Bartłomiej "PokerFacowaty" Nowak <pokerfacowaty.bn@gmail.com>
2025-04-20 21:09:45 +0200

    Added thefuck aliases

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,3 +1,9 @@
+if command -v thefuck 2>&1 >/dev/null
+then
+    eval $(thefuck --alias)
+fi
+
+alias f="fuck"
 alias ss="ssh pokerfacowaty@212.7.200.55 -p 2000"
 alias p="python3"
 alias ll="ls -alh"
@@ -9,3 +15,4 @@ alias v="vim"
 alias ebook-convert="/var/lib/flatpak/app/com.calibre_ebook.calibre/x86_64/stable/active/files/lib/calibre/ebook-convert"
 
 alias config='/usr/bin/git --git-dir=$HOME/.configs/ --work-tree=$HOME'
+
